7bae415897e2bda23b7406f81d2ef796109e4b06
viastolfi <vincent.astolfi12@gmail.com>
2025-09-30 16:29:09 +0200

    fix: use new Pixel way to create Shape

--- example/squares/main.cpp
+++ example/squares/main.cpp
@@ -3,8 +3,8 @@
 int main(int argc, char** argv)
 {
     Image* i = new Image("squares.ppm", 1920, 1080);
-    Shape* s1 = new Square(Vertice(400,400), Vertice(800,800), Colors::BLUE);
-    Shape* s2 = new Square(Vertice(500,500), Vertice(900,900), Colors::RED);
+    Shape* s1 = new Square(Pixel(400,400), Pixel(800,800), Colors::BLUE);
+    Shape* s2 = new Square(Pixel(500,500), Pixel(900,900), Colors::RED);
 
     i->draw(s1);
     i->draw(s2); 
