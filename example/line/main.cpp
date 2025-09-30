43c1d7c7429321c317c96a12043a6c0e1538864a
viastolfi <vincent.astolfi12@gmail.com>
2025-09-30 16:30:04 +0200

    fix: use new Pixel way to create Shape

--- example/line/main.cpp
+++ example/line/main.cpp
@@ -4,9 +4,8 @@ int main(int argc, char** argv)
 {
     Image* i = new Image("line.ppm", 1920, 1080);
 
-    // Edge(Vector2 startPos, Vector2 endPos)
-    Shape* edge = new Edge(Vertice(0, 10), Vertice(100, 40));
-    Shape* edge2 = new Edge(Vertice(500, 200), Vertice(500, 800), Colors::RED);
+    Shape* edge = new Edge(Pixel(0, 10), Pixel(100, 40));
+    Shape* edge2 = new Edge(Pixel(500, 200), Pixel(500, 800), Colors::RED);
     
     i->draw(edge); 
     i->draw(edge2);
