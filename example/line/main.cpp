3f0085669021135f6802612cafbbf45af0fb0644
viastolfi <vincent.astolfi12@gmail.com>
2025-10-01 16:11:53 +0200

    feat: update example with new API

--- example/line/main.cpp
+++ example/line/main.cpp
@@ -6,12 +6,20 @@ using koolos::Pixel;
 int main(int argc, char** argv)
 {
     koolos::Image* i = new koolos::Image("line.ppm", 1920, 1080);
+    koolos::Drawer* d = new koolos::Drawer();
+  
+    i->attachDrawer(d);
 
-    koolos::Shape* edge = new koolos::Edge(Pixel(0, 10), Pixel(100, 40));
-    koolos::Shape* edge2 = new koolos::Edge(Pixel(500, 200), Pixel(500, 800), Colors::RED);
+    d->strokeColor(Colors::BLUE);
+    d->moveTo(Pixel(0,10))->lineTo(Pixel(100, 40));
+    d->moveTo(Pixel(500, 200))->lineTo(Pixel(500, 800), Colors::RED);
+
+    /*
+     * this is the strict equivalent at what we were doing before but with more fancy and intuitive API
+     * koolos::Shape* edge = new koolos::Edge(Pixel(0, 10), Pixel(100, 40));
+     * koolos::Shape* edge2 = new koolos::Edge(Pixel(500, 200), Pixel(500, 800), Colors::RED);
+    */
     
-    i->draw(edge); 
-    i->draw(edge2);
     i->generate();
 
     return 0;
